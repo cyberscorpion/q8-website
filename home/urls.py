2dcc4b54ef9dcc7827388c9db98c823322b2436f
Rajat Jain <mznrajatjain@gmail.com>
2019-05-09 01:48:28 +0530

    navbar updated and pages added

--- home/urls.py
+++ home/urls.py
@@ -4,5 +4,10 @@ from django.views.generic import TemplateView
 urlpatterns = [
 
     path('', TemplateView.as_view(template_name='home.html')),
+    path('about-us', TemplateView.as_view(template_name='about-us.html')),
+    path('services', TemplateView.as_view(template_name='services.html')),
+    path('services-1', TemplateView.as_view(template_name='services-1.html')),
+    path('services-2', TemplateView.as_view(template_name='services-2.html')),
+    path('services-3', TemplateView.as_view(template_name='services-3.html')),
 
 ]
